863022e8d7ca1e7fe0a30ca11171d384483dab26
jiayun <jiayun_work@163.com>
2026-08-20 17:19:54 +0800

    fix: JYY-0220

--- internal/information/rsq.go
+++ internal/information/rsq.go
@@ -3,8 +3,8 @@ package information
 func adjPenalty(n, k int) float64 {
 	nf := float64(n)
 	kf := float64(k)
-	if nf-1 == 0 {
+	if nf-kf-1 <= 0 {
 		return 0
 	}
-	return (nf - kf - 1) / (nf - 1)
+	return (nf - 1) / (nf - kf - 1)
 }
