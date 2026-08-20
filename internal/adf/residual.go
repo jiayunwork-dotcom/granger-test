2e2a06ad220468c90cf03b8dde64ec092c700c55
jiayun <jiayun_work@163.com>
2026-08-20 17:17:57 +0800

    fix: JYY-0212

--- internal/adf/residual.go
+++ internal/adf/residual.go
@@ -1,13 +1,13 @@
 package adf
 
 func residualVariance(rss float64, m, k int) float64 {
-	_ = k
-	if m <= 0 {
+	df := m - k
+	if df <= 0 {
 		return 0
 	}
-	return rss / float64(m)
+	return rss / float64(df)
 }
 
 func unitRootCutoff(_ int) float64 {
-	return 0
+	return -2.86
 }
